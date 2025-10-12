f1a1da826c6d6402fb2b5e63133624ebb2b88848
Raily <rrraily@yandex.ru>
2025-10-12 21:06:36 +0300

    Телеграм бот

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import  Command
+from aiogram import F
+import asyncio
+
+load_dotenv()
+BOT_TOKEN = os.getenv("TG_API_KEY")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+@dp.message(Command("start"))
+async def start_handler(message: types.Message):
+    await message.answer("Привет! Ты запустил бота")
+
+async def main():
+    print("Бот запущен...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
